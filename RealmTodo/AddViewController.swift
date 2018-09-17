e7ae4ae721fe8bec9f8779a677f748a0a5d49e61
小西壮 <>
2018-09-17 14:27:19 +0800

    UI部品の設置

---
+++ RealmTodo/AddViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddViewController.swift
+//  RealmTodo
+//
+//  Created by 小西壮 on 2018/09/17.
+//  Copyright © 2018年 小西壮. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapButton(_ sender: UIButton) {
+        
+        
+    }
+    
+}
